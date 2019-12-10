a6f73c411b43743458a608a4801233fe09f81f46
penseestroller <penseestroller@gmail.com>
2019-12-10 15:53:22 +0800

    新建APSDNC项目

---
+++ ASPDNC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPDNC {
+    class Program {
+        static async Task Main(string[] args) {
+            //Console.WriteLine("Hello World!");
+
+            HttpListener httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8000/");
+            httpListener.Start();
+            while (true) {
+                var context = await httpListener.GetContextAsync();
+                await context.Response.OutputStream.WriteAsync(Encoding.UTF8.GetBytes("Hello World!"));
+                context.Response.Close();
+            }
+        }
+    }
+}
